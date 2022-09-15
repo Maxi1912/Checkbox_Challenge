cb5e300f7bf94eabaccb1fe6d14712447ecfb765
Maxi1912 <maximus.sheva@gmail.com>
2022-09-15 14:48:33 +0300

    add listener

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const input = document.querySelectorAll('input');
-console.log(input);
+const input = document.querySelectorAll(".inbox input[type='checkbox']");
+// console.log(input)
+let lastChecked;
+function hadleCheck(e) {
+  lastChecked = this;
+  console.log(lastChecked);
+}
+
+input.forEach((checkbox) => {
+  checkbox.addEventListener('click', hadleCheck);
+});
