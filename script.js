88123fff21055f9e8daec768e23d5c1ea47320e9
Maxi1912 <maximus.sheva@gmail.com>
2022-09-15 23:30:48 +0300

    that's all

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ let lastChecked;
 
 function hadleCheck(e) {
   let inBetween = false;
-  if(e.shiftKey && this.checked) {
-    checkboxes.forEach(checkbox => {
-      console.log(checkbox);
-      if(checkbox === this || checkbox === lastChecked) {
-        inBetween = !inBetween
-        console.log('Our checked checkbok')
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      // console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        // console.log('Our checked checkbok');
       }
-    })
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
   }
   lastChecked = this;
   // console.log(e);
